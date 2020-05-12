84d045ba924b588742a4a5fbe07798c26801a9cf
Turgut Abdullayev <turqut.a.314@gmail.com>
2020-05-12 21:10:34 +0400

    staging all files

--- rscript.R
+++ rscript.R
@@ -1,4 +1,8 @@
 
+# git add -A && git commit -m 'staging all files'
+file.remove(list.files('docs',include.dirs = TRUE,full.names = TRUE))
+unlink("docs/posts", recursive = TRUE)  
+unlink("docs/site_libs", recursive = TRUE) 
 
 # dependecies 
 pkgs = renv::dependencies(path = '_posts',errors = 'ignored', dev = TRUE)
@@ -10,10 +14,11 @@ files = list.files(path = '_posts', pattern = '.Rmd',recursive = TRUE)
 files = paste('_posts',files, sep = '/')
 
 install.packages(unique(pkgs$Package))
+install.packages('remotes')
 remotes::install_github("JohnCoene/echarts4r.suite")
 
 for (i in 1:length(files)) {
-  rmarkdown::render(files[i],quiet = TRUE)
+  rmarkdown::render(files[i], quiet = TRUE)
   print(paste('Done',i,'post out of',length(files)))
 }
 
