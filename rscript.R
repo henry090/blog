cc0314687db36a4ce0aaa3683ac8664688767828
Turgut Abdullayev <turqut.a.314@gmail.com>
2020-05-12 19:51:14 +0400

    staging all files

---
+++ rscript.R
@@ -0,0 +1,20 @@
+
+
+# dependecies 
+installed.packages('renv')
+pkgs = renv::dependencies(path = '_posts',errors = 'ignored', dev = TRUE)
+
+
+# run and render posts
+files = list.files(path = '_posts', pattern = '.Rmd',recursive = TRUE)
+
+files = paste('_posts',files, sep = '/')
+
+install.packages(unique(pkgs$Package))
+
+for (i in 1:length(files)) {
+  rmarkdown::render(files[i],quiet = TRUE)
+  print(paste('Done',i,'post out of',length(files)))
+}
+
+rmarkdown::render_site()
